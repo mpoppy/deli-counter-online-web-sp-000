48a00cadfd17c59fc5a95e3aff6c34800a795274
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 04:54:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(deli)
 #  end
 #end
   deli_line = []
-  deli.each_with_index { |index,value| deli_line << value + index}
+  deli.each_with_index { |index,value| deli_line << value + index }
 end
 #i have an array - display the current perople in line ---
 #the line is currently. 1 name, 2 name, 3 name, etc.
