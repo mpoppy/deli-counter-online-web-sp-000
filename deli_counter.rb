182fa0d45ca27e87fc99e3fa431890140951fe81
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 04:46:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,13 +3,14 @@
 katz_deli = ["Grace", "Kent", "marc"]
 
 def line(deli)
-  if deli.empty?
-    puts "The line is currently empty."
-  end
-end
+#  if deli.empty?
+#    puts "The line is currently empty."
+#  end
+#end
 
   deli.each do |person|#index 0
     person << person[index]
   end
+end
 #i have an array - display the current perople in line ---
 #the line is currently. 1 name, 2 name, 3 name, etc.
