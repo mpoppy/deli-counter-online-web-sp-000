855037cba3880d89ff1e0bb1d6a72cc71d26a005
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 04:27:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,10 @@
 katz_deli = []
 
 def line(deli)
-  if deli.empty?
-    puts "The line is currently empty."
-  end
+#  if deli.empty?
+  #  puts "The line is currently empty."
+  #end
   for index in (0...deli.length)
     puts"The line is currently: #{[index]} + #{deli[index]}"
+  end
 end
