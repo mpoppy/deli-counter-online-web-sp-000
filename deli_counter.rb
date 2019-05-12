85c26bba5e14fa7ea15b6b01175fc431fa563efb
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 04:23:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,4 @@ katz_deli = []
 def line(deli)
   if deli.empty?
     puts "The line is currently empty."
-  elsif !deli.empty?
-    deli.each do |person|
-      position_in_line = "#{person[index]} + 1"
-  end
-  puts "The line is currently: "
 end
