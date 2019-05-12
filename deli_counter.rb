809e3ac001f1d2c8a13798db5210388076084f56
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 04:47:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,6 +11,7 @@ def line(deli)
   deli.each do |person|#index 0
     person << person[index]
   end
+  deli.map 
 end
 #i have an array - display the current perople in line ---
 #the line is currently. 1 name, 2 name, 3 name, etc.
