0e2ab8895832cb6998db9c2e4ed29f47d181e75d
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 04:58:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,13 +3,13 @@
 katz_deli = ["Grace", "Kent", "marc"]
 
 def line(deli)
-#  if deli.empty?
-#    puts "The line is currently empty."
-#  end
-#end
-  deli_line = []
-  deli.each_with_index { |value, index| deli_line << "#{index + 1} #{value}" }
-  deli_line
+  if deli.empty?
+    puts "The line is currently empty."
+  elsif !deli.empty?
+    eli_line = []
+    deli.each_with_index { |value, index| deli_line << "#{index + 1} #{value}" }
+    deli_line
+  end
 end
 #i have an array - display the current perople in line ---
 #the line is currently. 1 name, 2 name, 3 name, etc.
