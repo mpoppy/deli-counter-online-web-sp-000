74a1b7fe78bc3f3f89aa229329ff7dcd40011630
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 04:38:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,10 @@
 katz_deli = ["Grace", "Kent", "marc"]
 
 def line(deli)
-#  if deli.empty?
-  #  puts "The line is currently empty."
-  #end
-  for index in (0...deli.length)
-    return "#{index + 1} #{deli[index]}"
+  if deli.empty?
+    puts "The line is currently empty."
+  end
+   index in (0...deli.length)
+    position(index) = "#{index + 1} #{deli[index]}"
   end
 end
